8ad2da72d2222437b22fef7c2b54c120c8b6acf1
Rúbia Marques de Oliveira <rubiamarquesoliveira@gmail.com>
2022-02-01 10:49:58 -0300

    Add files via upload

---
+++ src/data/index.ts
@@ -0,0 +1,26 @@
+export default[
+    {
+        title: 'Concentre-se apenas no trabalho importante',
+        description:
+          '重要な仕事だけに集中してください。',
+      },
+      {
+        title: 'Diga adeus ao trabalho maçante',
+        description:
+          'Konzentrieren Sie sich nur auf wichtige Arbeiten.',
+      },
+      {
+        title: 'Armazenamento e compartilhamento de arquivos são só o começo',
+        description:
+          'Хранение файлов и совместное использование - это только начало.',
+      },
+      {
+        title: 'For everyone',
+        description: 'Ini untuk digunakan semua orang.',
+      },
+      {
+        title: 'Bottom of the page',
+        description:
+          'This section, as we can see, is the bottom of the page. Have fun over here! :)',
+      },
+];
